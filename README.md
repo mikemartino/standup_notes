5fe0cc2f68e43e794459cb354ea9e1d7282d3bf7
Mike Martino <mikemartino86@gmail.com>
2020-03-25 21:49:38 -0400

    Initial commit of standup-notes

---
+++ README.md
@@ -0,0 +1,16 @@
+## Install
+
+```
+pipenv install
+```
+
+## Run
+```
+pipenv run python3 notes --list
+pipenv run python3 notes --read-today
+pipenv run python3 notes --read-tomorrow
+pipenv run python3 notes --edit-today
+pipenv run python3 notes --edit-tomorrow
+pipenv run python3 notes --copy-today
+pipenv run python3 notes --copy-tomorrow
+```
---
+++ standup_notes/__main__.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+from argparse import ArgumentParser, Namespace
+import os
+from datetime import datetime, timedelta
+import pyperclip
+import editor
+
+EXT = '.standup-notes.txt'
+STANDUP_NOTES = os.path.join(os.environ.get("HOME"), 'Desktop/standup-notes')
+STANDUP_TEMPLATE = os.path.join(os.path.dirname(__file__), 'standup.template')
+
+
+def main(arguments: Namespace):
+    today = datetime.strftime(datetime.now(), '%Y%m%d')
+    today_note = os.path.join(STANDUP_NOTES, today + EXT)
+    tomorrow = datetime.strftime(datetime.now() + timedelta(days=1), '%Y%m%d')
+    tomorrow_note = os.path.join(STANDUP_NOTES, tomorrow + EXT)
+
+    if arguments.list:
+        for note in os.listdir(STANDUP_NOTES):
+            print(os.path.join(STANDUP_NOTES, note))
+
+    if arguments.read_today:
+        read_note(today_note)
+
+    if arguments.read_tomorrow:
+        read_note(tomorrow_note)
+
+    if arguments.edit_today:
+        edit_note(today_note)
+
+    if arguments.edit_tomorrow:
+        edit_note(tomorrow_note)
+
+    if arguments.copy_today:
+        copy_note(today_note)
+
+    if arguments.copy_tomorrow:
+        copy_note(tomorrow_note)
+
+
+def copy_note(note):
+    """Copy the note to the clipboard if it exists."""
+    if os.path.exists(note):
+        with open(note, 'r') as f:
+            pyperclip.copy(f.read())
+    else:
+        print(note + ' doesn\'t exist yet.')
+
+
+def edit_note(note):
+    """Launches the default $EDITOR or a suitable default. If the note already exists, the editor opens it for editing.
+    If the note does not exist, the editor opens a new file using the stand-up template."""
+    if os.path.exists(note):
+        editor.edit(note)
+    else:
+        with open(STANDUP_TEMPLATE, 'r') as f:
+            editor.edit(note, f.read())
+
+
+def read_note(note):
+    """Print the note if it exists."""
+    if os.path.exists(note):
+        with open(note, 'r') as f:
+            print(f.read())
+    else:
+        print(note + ' doesn\'t exist yet.')
+
+
+parser = ArgumentParser()
+parser.add_argument('--list', help='List all stand-up notes.', action='store_true')
+parser.add_argument('--read-today', help='Read today\'s stand-up notes.', action='store_true')
+parser.add_argument('--read-tomorrow', help='Read tomorrow\'s stand-up notes.', action='store_true')
+parser.add_argument('--copy-today', help='Copy today\'s stand-up notes to the clipboard.', action='store_true')
+parser.add_argument('--copy-tomorrow', help='Copy tomorrow\'s stand-up notes to the clipboard.', action='store_true')
+parser.add_argument('--edit-today', help='Edit today\'s stand-up notes.', action='store_true')
+parser.add_argument('--edit-tomorrow', help='Edit tomorrow\'s stand-up notes.', action='store_true')
+args = parser.parse_args()
+
+main(args)
