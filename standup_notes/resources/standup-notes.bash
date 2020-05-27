c5c63be5cb4ac0898f2fddcfa4281aaf5174174a
marcdonovan <marcdonovan@cmail.carleton.ca>
2020-05-27 13:05:05 -0400

    - fixed bash script to included copy flag and reduce other inputs

--- standup_notes/resources/standup-notes.bash
+++ standup_notes/resources/standup-notes.bash
@@ -2,36 +2,32 @@
 
 _standup_completions()
 {
-    local cur prev opts
+    local cur prev opts base
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    first_arg="${COMP_WORDS[1]}"
+    prev2="${COMP_WORDS[COMP_CWORD-2]}"
 
-    opts="-l --list -v --view -c --copy -e --edit -d --delete"
+    opts="-l --list -r --read -c --copy -e --edit -d --delete"
     days="--today --tomorrow --yesterday"
 
     case "${prev}" in
       -l | --list | -d | --delete | --today | --tomorrow | --yesterday)
-        case "${first_arg}" in
-          -e | --edit)
-          COMPREPLY=($(compgen -W "-c --copy" -- ${cur}))
-          return 0
-          ;;
-        esac
+        if [ "$prev2" = "-e" ] || [ "$prev2" = "--edit" ]; then
+          COMPREPLY=( $(compgen -W "-c --copy" -- ${cur}))
+        fi
         return 0
         ;;
-      -c | --copy| -e | --edit | -v | --view)
-      if [ "${#COMP_WORDS[@]}" != "3" ]; then
-        return
-      fi
+      -c | --copy | -e | --edit | -r | --read)
+        if [ "$prev2" = "--yesterday" ] || [ "$prev2" = "--today" ] ||[ "$prev2" = "--tomorrow" ] ; then
+          return 0
+        fi
         COMPREPLY=( $(compgen -W "${days}" -- ${cur}))
         return 0
         ;;
       *)
       ;;
     esac
-
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}))
 
 
